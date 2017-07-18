a8d6a64d681617cb2adf945f3153eec0cee3f654
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-07-18 13:07:50 +0300

    добавила скрипт открывания/закрытия деталей фермента

--- project/app/js/scripts.js
+++ project/app/js/scripts.js
@@ -1,1 +1,39 @@
 'use strict';
+var allFerments = document.querySelectorAll( '.ferment' ),
+	allFermentsDetails = document.querySelectorAll( '.ferment-details' ),
+	allCloseLinks = document.querySelectorAll( '.ferment-details__close' );
+
+// событие клика
+
+for( var i = 0; i < allFerments.length; i++) {
+	allFerments[i].addEventListener( 'click', showFermentDetails )
+	};
+
+for ( var k = 0; k < allCloseLinks.length; k++ ) {
+	allCloseLinks[k].addEventListener( 'click', hideFermentDetails )
+	};
+
+function showFermentDetails(e) {
+	// получили элемент, по которому кликнули, прочли его ID
+	var targetFermentID = e.currentTarget.id;
+	console.log( targetFermentID );
+	// нашли по этому ID подходящий ferment-description
+	for( var i = 0; i < allFermentsDetails.length; i++ ) {
+		if ( allFermentsDetails[i].getAttribute('data-ferment') == targetFermentID ) {
+			console.log( allFermentsDetails[i] );
+			allFermentsDetails[i].classList.remove( 'ferment-details--hidden' );
+			allFermentsDetails[i].classList.add( 'ferment-details--visible' );
+			}
+		} 
+	};
+
+function hideFermentDetails(e) {
+	var elementToClose = this.parentNode.parentNode;
+	elementToClose.classList.remove( 'ferment-details--visible' );
+	elementToClose.classList.add( 'ferment-details--hidden' );
+	};
+
+// открытие-закрытие модального окна с описанием фермента
+
+// переключение выбранного типа реакций
+// отбор ферментов в зависимости от выбранного типа реакции
